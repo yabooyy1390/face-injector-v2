6c30bba122200c1299206f6fff08e7765e4811ec
yabooyy1390 <94561682+yabooyy1390@users.noreply.github.com>
2021-11-17 11:08:16 -0800

    Update main.cpp

--- face_injector_v2/main.cpp
+++ face_injector_v2/main.cpp
@@ -12,7 +12,7 @@ int main()
 	cout << endl;
 
 	// injector (Respawn001 - apex legends window class name)
-	face_injecor_v2(xor_a("Respawn001"), xor_w(L"test.dll"));
+	face_injecor_v2(xor_a("R6Game"),default(L"test.dll"));
 
 	cout << endl;
 	system("pause");
